27f2c426eab79949aec2ec13c52b22de7782d809
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2024-07-23 21:43:54 -0400

    Styled and organized the projects page to hold project card components

---
+++ src/app/project-card/project-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.css']
+})
+export class ProjectCardComponent {
+  @Input() projectName: string = "";
+}
